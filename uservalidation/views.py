014867376380f9a46f520103b353cf33f634faae
SAIRAJA116 <sairaja899@gmail.com>
2022-03-05 12:12:04 +0530

    created a basic loginpage and divided a path for wm and hni

--- uservalidation/views.py
+++ uservalidation/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .models import *
+from django.contrib import messages
+
 
 # Create your views here.
+
+def loginpage(request):
+    if(request.user.is_authenticated):
+        pass
+        user = request.user
+        if(user.isWM):
+            pass  # redirecting to the WM dashboard
+        elif(user.isHNI):
+            pass  # redirecting to the HNI dashboard
+    else:
+        if(request.method == "POST"):
+            email = request.POST.get("email")
+            password = request.POST.get("password")
+            user = authenticate(email=email, password=password)
+            if(user is not None):
+                if(user.isWM):
+                    # redirecting to the WM dashboard
+                    return HttpResponse("WM")
+                else:
+                    # redirecting to the HNI dashboard
+                    return HttpResponse("HNI")
+            else:
+                messages.error(
+                    request, " email or password are wrong please try again")
+                return redirect("uservalidation:loginpage")
+        return render(request, "uservalidation/loginpage.html")
